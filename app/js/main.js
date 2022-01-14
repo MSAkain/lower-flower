9939829828591840e72cb0d20a6e8a616218616e
MSAkain <theggodisthere@gmail.com>
2022-01-14 08:55:36 +0300

    added 2 sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,13 @@
 $(function () {
 
 
-	$('.reviews__slider').slick({
-		fade: true,
-		infinite: false,
-		prevArrow: '<button type="button" class="slick-arrow arrow-right"><img src="images/icons/arrow-1.svg" alt=""></button>',
-		nextArrow: '<button type="button" class="slick-arrow arrow-left"><img src="images/icons/arrow-2.svg" alt=""></button>',
+	new Swiper('.popular__swiper', {
+		slidesPerView: 3,
+		spaceBetween: 20,
+		navigation: {
+			nextEl: '.swiperNext',
+			prevEl: '.swiperPrev'
+		},
 	});
-
-
 
 });
