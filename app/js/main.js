2c97bf91f57b3873b834055613aeb29ab55ac1cd
MSAkain <theggodisthere@gmail.com>
2022-01-21 10:46:43 +0300

    added scroll header and busket

--- app/js/main.js
+++ app/js/main.js
@@ -10,4 +10,45 @@ $(function () {
 		},
 	});
 
+	$('.ticket__input').styler();
+
+	const $header = $(".header-scroll")
+	const scroll = 0
+	const active = "header-scroll--active"
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > scroll) {
+			$header.addClass(active)
+		} else {
+			$header.removeClass(active)
+		}
+	})
+
+	$('.header__basket').on('click', function () {
+		$('.busket').addClass('busket--active')
+		$('.header').addClass('header--blur')
+		$('body').addClass('hidden')
+	});
+
+	$('.header-scroll__button').on('click', function () {
+		$('.busket').addClass('busket--active')
+		$('.header').addClass('header--blur')
+		$('body').addClass('hidden')
+	});
+
+	$('.busket__close').on('click', function () {
+		$('.busket').removeClass('busket--active')
+		$('.header').removeClass('header--blur')
+		$('body').removeClass('hidden')
+	});
+
+	$(document).mouseup(function (e) { // событие клика по веб-документу
+		var div = $(".busket"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+			&& div.has(e.target).length === 0) { // и не по его дочерним элементам
+			div.removeClass('busket--active')
+			$('.header').removeClass('header--blur')
+			$('body').removeClass('hidden')
+		}
+	});
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,8 @@ function scripts() {
 	return src([
 		'node_modules/jquery/dist/jquery.js',
 		'node_modules/swiper/swiper-bundle.min.js',
+		'node_modules/select2/dist/js/select2.js',
+		'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
 		'node_modules/slick-carousel/slick/slick.js',
 		'app/js/main.js'
 	])
