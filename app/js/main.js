bdbbc224ee3cd17c69688b92677b367c1b814fc5
MSAkain <theggodisthere@gmail.com>
2022-01-24 08:48:05 +0300

    added js scripts and changed scroll-input

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function () {
 
+	const lastScrollTop = 0;
+	const headerScroll = document.querySelector('.header-scroll');
+	const body = document.querySelector('body');
+	const headerBusket = document.querySelector('.header__basket');
+	const busket = document.querySelector('.busket');
+	const busketClose = document.querySelector('.busket__close');
+	const header = document.querySelector('.header');
+	const headerScrollButton = document.querySelector('.header-scroll__button');
+	const headerScrollInput = document.querySelector('.header-scroll__input');
+	const headerScrollForm = document.querySelector('.header-scroll__form');
+	const headerScrollSearch = document.querySelector('.header-scroll__search');
+	const headerScrollClose = document.querySelector('.header-scroll__close');
+	const headerScrollRef = document.querySelector('.header-scroll__ref');
+
+
 
 	new Swiper('.popular__swiper', {
 		slidesPerView: 3,
@@ -12,43 +27,63 @@ $(function () {
 
 	$('.ticket__input').styler();
 
-	const $header = $(".header-scroll")
-	const scroll = 0
-	const active = "header-scroll--active"
-	$(window).scroll(function () {
-		if ($(window).scrollTop() > scroll) {
-			$header.addClass(active)
+
+
+
+	window.addEventListener("scroll", function () {
+		const st = window.pageYOffset || document.documentElement.scrollTop;
+
+		if (st > lastScrollTop) {
+
+			headerScroll.classList.add("header-scroll--active");
 		} else {
-			$header.removeClass(active)
+
+			headerScroll.classList.remove("header-scroll--active");
 		}
+	});
+
+	headerBusket.addEventListener('click', function () {
+		busket.classList.add('busket--active');
+		header.classList.add('header--blur');
+		body.classList.add('hidden');
+	});
+
+	headerScrollButton.addEventListener('click', function () {
+		busket.classList.add('busket--active');
+		header.classList.add('header--blur');
+		body.classList.add('hidden');
+	});
+
+	busketClose.addEventListener('click', function () {
+		busket.classList.remove('busket--active');
+		header.classList.remove('header--blur');
+		body.classList.remove('hidden');
+	});
+
+	window.addEventListener('click', function (e) {
+		if (!busket.contains(e.target) && !headerBusket.contains(e.target) && !headerScrollButton.contains(e.target)) {
+			busket.classList.remove('busket--active');
+			header.classList.remove('header--blur');
+			body.classList.remove('hidden');
+		}
+	});
+
+	headerScrollInput.addEventListener('click', function () {
+		headerScrollInput.classList.add('header-scroll__input--active');
+		headerScrollForm.classList.add('header-scroll__form--active');
+		headerScrollSearch.classList.add('header-scroll__search--active');
+		headerScrollClose.classList.add('header-scroll__close--active');
+		headerScrollRef.classList.add('header-scroll__ref--disabled');
+		document.getElementsByName('search')[0].placeholder = 'Введите свой запрос';
 	})
 
-	$('.header__basket').on('click', function () {
-		$('.busket').addClass('busket--active')
-		$('.header').addClass('header--blur')
-		$('body').addClass('hidden')
-	});
-
-	$('.header-scroll__button').on('click', function () {
-		$('.busket').addClass('busket--active')
-		$('.header').addClass('header--blur')
-		$('body').addClass('hidden')
-	});
-
-	$('.busket__close').on('click', function () {
-		$('.busket').removeClass('busket--active')
-		$('.header').removeClass('header--blur')
-		$('body').removeClass('hidden')
-	});
-
-	$(document).mouseup(function (e) { // событие клика по веб-документу
-		var div = $(".busket"); // тут указываем ID элемента
-		if (!div.is(e.target) // если клик был не по нашему блоку
-			&& div.has(e.target).length === 0) { // и не по его дочерним элементам
-			div.removeClass('busket--active')
-			$('.header').removeClass('header--blur')
-			$('body').removeClass('hidden')
-		}
-	});
+	headerScrollClose.addEventListener('click', function () {
+		headerScrollInput.classList.remove('header-scroll__input--active');
+		headerScrollForm.classList.remove('header-scroll__form--active');
+		headerScrollSearch.classList.remove('header-scroll__search--active');
+		headerScrollClose.classList.remove('header-scroll__close--active');
+		headerScrollRef.classList.remove('header-scroll__ref--disabled');
+		document.getElementsByName('search')[0].placeholder = 'Поиск';
+	})
 
 });
